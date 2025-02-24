fa8dd24efa238c8b70a06b022d0a5795379647cf
Jules <julesgrange@outlook.fr>
2025-02-24 14:23:15 +0100

    update on mainPage and footer css.

--- site/src/pages/Homepage.tsx
+++ site/src/pages/Homepage.tsx
@@ -8,32 +8,26 @@ import { NavLink} from "react-router-dom";
 const Homepage = () => {
     return ( 
         <>
-        <div className="homePage-container">
+        <div className="homePage">
             <h1 className="title">SpeedyWiki</h1>
             <br />
-            <div className="homePage-size">
-
+            <div className="homePage-container">
                 <div className="homePage-top">
-                    <div className="homePage_icons-container">
+                    <div className="icons-container">
                         <img className='icon' src={Reduce}></img>
                         <img className='icon' src={Full}></img>
                         <img className='icon' src={Cross}></img>
                     </div>
                 </div>
-
                 <div className="homePage-content">
-
                     <h1>Create or Join an online lobby !</h1>
                     <div className="button_links">
-                        <NavLink to="/NameSelectionHost"className="home_button">Create !</NavLink>
+                        <NavLink to="/NameSelectionHost" className="home_button">Create !</NavLink>
                         <NavLink to="/Join" className="home_button">Join !</NavLink>
                     </div>
-
                 </div>
             </div>
-        </div>
-        <div className='Footer'>
-            <FooterWindow />
+            <FooterWindow/>
         </div>
         </>
         )
