493a92860862b28e149963032487f9087430d2dc
Jules <julesgrange@outlook.fr>
2025-02-24 14:45:26 +0100

    fixed bug on character selection (now prevents having the same one twice in a row).

--- site/src/components/Pic_selec.tsx
+++ site/src/components/Pic_selec.tsx
@@ -9,30 +9,29 @@ import PP6 from '../assets/Image/New Piskel 3.png';
 
 const PicSelect = () => {
     const PP = [PP1,PP2,PP3,PP4,PP5,PP6];
-    const [randomItem, setRandomItem] = useState(PP[Math.floor(Math.random() * PP.length)]);
+    const [item, setItem] = useState(PP[Math.floor(Math.random() * PP.length)]);
 
     const generateRandomItem = () => {
-      const randimage = PP[Math.floor(Math.random() * PP.length)]
-      if (randimage==randomItem){
+      const randomItem = PP[Math.floor(Math.random() * PP.length)]
+      if (randomItem == item){
         generateRandomItem();
       } else {
-        setRandomItem(PP[Math.floor(Math.random() * PP.length)]);
+        setItem(randomItem);
       }
     };
     return(
-        <><div className="flex flex-col items-center">
-        <div className="flex flex-col items-center relative">
-          {/* Image */}
-          <img className="h-[210px] w-[210px]" src={randomItem} alt="random" />
-    
-          {/* Bouton aligné en bas à droite sans absolute */}
-          <div className="flex justify-end w-full mt-[-40px] pr-2">
-            <button className="h-[50px] w-[50px] z-10" onClick={generateRandomItem}>
-              <img src={Randicon} alt="random icon" />
-            </button>
-          </div>
-        </div>
-      </div>
+        <>
+            <div className="container">
+                <div className="image-container">
+                    <img className="image" src={item} alt="random" />
+                    <div className="button-container">
+                        <button className="button" onClick={generateRandomItem}>
+                            <img src={Randicon} alt="random icon" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 };
