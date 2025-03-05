39d7ca934ff9608514c01bbe2329639b0b52fb86
Guillaume <guillaume.augeraud@edu.univ-eiffel.fr>
2025-03-05 12:02:30 +0100

    Creation of Start.js

---
+++ Start.js
@@ -0,0 +1,24 @@
+const { exec } = require("child_process");
+const path = require("path");
+
+// Fonction pour exécuter une commande dans un dossier spécifique
+const runCommand = (command, location) => {
+  const process = exec(command, { cwd: location });
+
+  process.stdout.on("data", (data) => {
+    console.log(`[${location}] ${data}`);
+  });
+
+  process.stderr.on("data", (data) => {
+    console.error(`[${location} ERROR] ${data}`);
+  });
+  process.on("close", (code) => {
+    console.log(`[${location}] Process exited with code ${code}`);
+  });
+};
+
+// Démarrer le backend (Node.js)
+runCommand("node server.js", path.join(__dirname, "backend"));
+runCommand("node websocket.js", path.join(__dirname, "backend"));
+// Démarrer le frontend (React)
+runCommand("npm run dev", path.join(__dirname, "site"));
