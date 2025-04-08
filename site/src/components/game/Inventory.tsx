6f1d5a9b43ab996a4be2f8ce36ab20c699124389
randy <randyboujaber@hotmail.fr>
2025-04-08 10:50:20 +0200

    Artifacts now have the right icon and name, and get added to inventory correctly.

--- site/src/components/game/Inventory.tsx
+++ site/src/components/game/Inventory.tsx
@@ -1,26 +1,7 @@
 import { Artifact } from "../../interfaces/Artifact";
-import TEMP from "../../assets/image/artifact/TEMP.png";
 
 export const baseInventory: Artifact[] = [
-    {
-        id:0,
-        name: "test1",
-        icon: TEMP,
-        effect: 0,
-        count: 5,
-    },{
-        id:1,
-        name: "test2",
-        icon: TEMP,
-        effect: 1,
-        count: 2,
-    },{
-        id:2,
-        name: "test3",
-        icon: TEMP,
-        effect: 2,
-        count: 3,
-    }
+
 ];
 
 export default baseInventory;
